9fecfb91e50b02e699f64d1b2471080700c274e7
nhemmings <natehemm@gmail.com>
2017-10-25 18:17:45 -0500

    Completed through lecture 22

--- Assets/scripts/NumberWizard.cs
+++ Assets/scripts/NumberWizard.cs
@@ -3,36 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-    int min = 1;
-    int max = 1000;
-    int guess = 500;
+    int min, max, guess;
 
     // Use this for initialization
     void Start () {
-        print("Welcome to Number Wizard");
-        print("Pick a number in your head, but don't tell me!");
-
-        print("The highest number you can pick is " + max);
-        print("The lowest number you can pick is " + min);
-
-        print("Is the number higher or lower than " + guess + "?");
-        print("Up = higher, down = lower, Enter = equal.");
-
-
+        StartGame();
 	}
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (max + min) / 2;
-            print("Is the number higher or lower than " + guess + "?");
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (max + min) / 2;
-            print("Is the number higher or lower than " + guess + "?");
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won!");
+            print("========================");
+            StartGame();
         }
+    }
+
+    void StartGame() {
+        min = 0;
+        max = 1000;
+        guess = 500;
+
+        print("Welcome to Number Wizard");
+        print("Pick a number in your head, but don't tell me!");
+
+        print("The lowest number you can pick is " + min);
+        print("The highest number you can pick is " + max);
+
+        max = (max % 2 == 0) ? ++max : max; // Integer rounding will prevent guesses of max if max is even.
+                                            // Incrementing max to an odd number resolves the issue
+
+        print("Is the number higher or lower than " + guess + "?");
+        print("Up = higher, down = lower, Enter = equal.");
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        print("Is the number higher or lower than " + guess + "?");
+        print("Up = higher, down = lower, Enter = equal.");
     }
 }
