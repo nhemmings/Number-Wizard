ea60dee2f4299c1d47d61f82d06e279a18a5b727
Nathan Hemmings <natehemm@gmail.com>
2017-10-25 12:10:54 -0500

    Completed through lecture 20

--- Assets/scripts/NumberWizard.cs
+++ Assets/scripts/NumberWizard.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         print("Welcome to Number Wizard");
         print("Pick a number in your head, but don't tell me!");
-
-        int min = 1;
-        int max = 1000;
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + "?");
         print("Up = higher, down = lower, Enter = equal.");
 
 
@@ -24,9 +24,13 @@ public class NumberWizard : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            print("Up arrow pressed");
+            min = guess;
+            guess = (max + min) / 2;
+            print("Is the number higher or lower than " + guess + "?");
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            print("Down arrow pressed");
+            max = guess;
+            guess = (max + min) / 2;
+            print("Is the number higher or lower than " + guess + "?");
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won!");
         }
